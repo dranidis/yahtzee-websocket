358ad1b6e36622bbf819d4fdf44d48cff0948331
dranidis <dranidis@gmail.com>
2020-01-10 20:51:49 +0200

    Move Application to correct folder

--- src/main/java/com/asdt/yahtzee/Application.java
+++ src/main/java/com/asdt/yahtzee/Application.java
@@ -1,4 +1,4 @@
-package com.asdt.yahtzee.websocket;
+package com.asdt.yahtzee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
